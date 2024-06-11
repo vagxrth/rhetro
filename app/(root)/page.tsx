b4576f7ce8c34255de20141a189ffdcfaf0a0e42
Vagarth <vagarth419@gmail.com>
2024-06-11 20:17:30 +0530

    feat: add homepage layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import PodcastCard from '@/components/PodcastCard'
 import { Button } from '@/components/ui/button'
+import { podcastData } from '@/constants'
 import React from 'react'
 
 const Home = () => {
@@ -6,6 +8,11 @@ const Home = () => {
     <div className='mt-9 flex flex-col gap-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Premier Podcasts</h1>
+        <div className='podcast_grid'>
+          {podcastData.map(({ id, title, description, imgURL }) => (
+            <PodcastCard key={id} imgURL={imgURL} title={title} description={description} podcastId={id} />
+          ))}
+        </div>
       </section>
     </div>
   )
---
+++ components/RightSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+    <section className='right_sidebar text-white-1'>
+        Right Sidebar
+    </section>
+  )
+}
+
+export default RightSidebar
